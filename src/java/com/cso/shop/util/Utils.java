56c626c39f7d11ab672208c4eac8d04f62ab19ab
Cactus2357 <baotqhe181879@fpt.edu.vn>
2024-10-13 18:02:04 +0700

    add random string utility

--- src/java/com/cso/shop/util/Utils.java
+++ src/java/com/cso/shop/util/Utils.java
@@ -5,7 +5,16 @@
 package com.cso.shop.util;
 
 import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.Provider;
+import java.security.Provider.Service;
+import java.security.SecureRandom;
+import java.security.Security;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -13,12 +22,20 @@ import java.security.MessageDigest;
  */
 public class Utils {
 
+  public static void main(String[] args) {
+
+  }
+
   /**
    * Hash with SHA-256
    */
   public static String hash(String string) {
+    return hash(string, "SHA-256");
+  }
+
+  public static String hash(String string, String algorithm) {
     try {
-      MessageDigest md = MessageDigest.getInstance("SHA-256");
+      MessageDigest md = MessageDigest.getInstance(algorithm);
       ByteArrayInputStream fis = new ByteArrayInputStream(string.getBytes());
 
       byte[] dataBytes = new byte[1024];
@@ -44,7 +61,7 @@ public class Utils {
       return sb.toString();
     } catch (Exception e) {
       System.err.println(e);
-      return string;
+      return null;
     }
   }
 
@@ -61,6 +78,45 @@ public class Utils {
       return Double.parseDouble(value);
     } catch (NumberFormatException e) {
       return defaultVal;
+    }
+  }
+
+  public static String generateToken(int length) {
+    byte[] bytes = new byte[20];
+    try {
+      SecureRandom.getInstanceStrong().nextBytes(bytes);
+      String token = bytesToHex(bytes);
+      return token;
+    } catch (NoSuchAlgorithmException ex) {
+      Logger.getLogger(Utils.class.getName()).log(Level.SEVERE, null, ex);
+      return null;
+    }
+  }
+
+  private static final byte[] HEX_ARRAY = "0123456789ABCDEF".getBytes(StandardCharsets.US_ASCII);
+
+  public static String bytesToHex(byte[] bytes) {
+    byte[] hexChars = new byte[bytes.length * 2];
+    for (int j = 0; j < bytes.length; j++) {
+      int v = bytes[j] & 0xFF;
+      hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+      hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+    }
+    return new String(hexChars, StandardCharsets.UTF_8);
+  }
+
+  private static void getAlgorithms() {
+    Provider[] providers = Security.getProviders();
+    for (Provider p : providers) {
+      String providerStr = String.format("%s/%s/%f\n", p.getName(),
+        p.getInfo(), p.getVersion());
+      Set<Service> services = p.getServices();
+      for (Service s : services) {
+        if ("MessageDigest".equals(s.getType())) {
+          System.out.printf("\t%s/%s/%s\n", s.getType(),
+            s.getAlgorithm(), s.getClassName());
+        }
+      }
     }
   }
 }
